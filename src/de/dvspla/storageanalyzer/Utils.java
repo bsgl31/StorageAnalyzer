d595ab1fbdb7f41eea47220ddac757501c2881be
mpuchner <maxi-puchner@outlook.de>
2022-01-28 09:11:07 +0100

    Added java documentation for Utils.java class

--- src/de/dvspla/storageanalyzer/Utils.java
+++ src/de/dvspla/storageanalyzer/Utils.java
@@ -14,6 +14,11 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Utils {
 
+    /**
+     * Berechnet die Gesamtgröße eines Pfades und dessen Dateien/Ordner rekursiv
+     * @param file Zu berechnender Pfad
+     * @return Gesamtgrößer des übergebenen Pfades in bytes
+     */
     public static long getFileSize(File file) {
         final AtomicLong amount = new AtomicLong(0);
 
@@ -42,7 +47,10 @@ public class Utils {
         return amount.get();
     }
 
-
+    /**
+     * Öffnet die übergebene Datei.
+     * @param file Zu öffnende Datei
+     */
     public static void openFile(File file) {
         try {
             Desktop.getDesktop().open(file);
@@ -51,6 +59,10 @@ public class Utils {
         }
     }
 
+    /**
+     * Öffnet den übergeordneten Ordner der übergebenen Datei und wählt sie im Explorer an.
+     * @param file Auszuwählende Datei
+     */
     public static void selectFileInExplorer(File file) {
         try {
             Runtime.getRuntime().exec("explorer.exe /select, \"" + file.getAbsolutePath() + "\"");
@@ -59,6 +71,10 @@ public class Utils {
         }
     }
 
+    /**
+     * Öffnet bzw. führt die übergebene Datei aus.
+     * @param file Auszuführende Datei
+     */
     public static void openFileInExplorer(File file) {
         try {
             Runtime.getRuntime().exec("explorer.exe /open, \"" + file.getAbsolutePath() + "\"");
@@ -71,12 +87,20 @@ public class Utils {
         JOptionPane.showMessageDialog(null, "An error has occurred.", "Error", JOptionPane.ERROR_MESSAGE);
     }
 
+    /**
+     * Löscht eine Datei aus dem Dateisystem
+     * @param file
+     */
     public static void deleteFile(File file) {
         if (!file.delete()) {
             showError();
         }
     }
 
+    /**
+     * Löscht den übergebenen Ordner rekursiv aus dem Dateisystem und der Dateiansicht im StorageAnalyzer.
+     * @param file Zu löschender Ordner
+     */
     public static void deleteDirectory(File file) {
         File[] files = file.listFiles();
         if (files == null || files.length == 0) return;
@@ -89,6 +113,11 @@ public class Utils {
         }
     }
 
+    /**
+     * Subtrahiert rekursiv die übergebene Dateigröße von allen übergeordneten Ordnern.
+     * @param parent Übergeordneter Ordner
+     * @param size Größe der zu entfernenden Datei
+     */
     private static void updateSize(TreeItem<SearchItem> parent, long size) {
         parent.getValue().setSize(parent.getValue().getBytes() - size);
         if (parent.getParent() != null) {
@@ -96,6 +125,10 @@ public class Utils {
         }
     }
 
+    /**
+     * Entfernt ein Element aus der Liste und aktualisiert mithilfe der {@link #updateSize updateSize} Methode die Speichergrößen der übergeordneten Ordner.
+     * @param view Zu entfernendes Element
+     */
     public static void removeSelectedAndUpdateSize(TreeTableView<SearchItem> view) {
         TreeItem<SearchItem> item = view.getSelectionModel().getSelectedItem();
         TreeItem<SearchItem> parent = item.getParent();
